a612b933e7af764cb67d6b913197fc9f3095de16
Alpesh <alpesh@pepo.com>
2018-05-02 21:05:02 +0530

    refactoring for error

--- tests/mocha/lib/formatter/response_helper.js
+++ tests/mocha/lib/formatter/response_helper.js
@@ -14,10 +14,13 @@ const api_error_key = Object.keys(apiErrorConfig)[0]
 ;
 
 const responseHelper = new OSTBase.responseHelper({
-  moduleName: 'openst-base',
+  moduleName: 'openst-base'
+});
+
+const errorConfig = {
   param_error_config: paramErrorConfig,
   api_error_config: apiErrorConfig
-});
+};
 
 const testHash = { 'test_key': 'test_value'};
 
@@ -60,10 +63,9 @@ describe('lib/formatter/response_helper', function(){
 
   it('Should have all the expected keys in response hash', function(){
 
-    let responseData = responseHelper.error('test_1', api_error_key, {}).toHash();
+    let responseData = responseHelper.error('test_1', api_error_key, {}).toHash(errorConfig);
 
     assert.equal(true, responseData.hasOwnProperty('success'));
-    assert.equal(true, responseData.hasOwnProperty('code'));
     assert.equal(true, responseData.hasOwnProperty('err'));
     assert.equal(true, responseData.err.hasOwnProperty('code'));
     assert.equal(true, responseData.err.hasOwnProperty('msg'));
@@ -74,10 +76,9 @@ describe('lib/formatter/response_helper', function(){
 
   it('Should have all the expected keys and values when called successWithData', function(){
 
-    let responseData = responseHelper.successWithData(testHash).toHash();
+    let responseData = responseHelper.successWithData(testHash).toHash(errorConfig);
 
     assert.equal(true, responseData.hasOwnProperty('success'));
-    assert.equal(true, responseData.hasOwnProperty('code'));
     assert.equal(true, responseData.hasOwnProperty('data'));
     assert.equal(true, responseData.data.hasOwnProperty('test_key'));
     assert.equal('test_value', responseData.data.test_key);
@@ -85,56 +86,51 @@ describe('lib/formatter/response_helper', function(){
   });
 
   it('Should return an array for param in err object', function(){
-    let responseData = responseHelper.paramValidationError('test_1', api_error_key, [param_error_key], {}).toHash();
+    let responseData = responseHelper.paramValidationError('test_1', api_error_key, [param_error_key], {}).toHash(errorConfig);
 
     assert.equal(true, responseData.err.param instanceof Array);
-  });
-
-  it('Should return code as per api_error_config in response hash', function(){
-
-    assert.equal(apiErrorConfig[api_error_key].http_code, responseHelper.error('test_1', api_error_key, {}).toHash().code);
   });
 
   it('Should return code in err as per input error code', function(){
 
     assert.equal(apiErrorConfig[api_error_key].code,
-      responseHelper.error('test_1', api_error_key, {}).toHash().err.code);
+      responseHelper.error('test_1', api_error_key, {}).toHash(errorConfig).err.code);
   });
 
   it('Should return msg in err as per input error code', function(){
 
     assert.equal(apiErrorConfig[api_error_key].message,
-      responseHelper.error('test_1', api_error_key, {}).toHash().err.msg);
+      responseHelper.error('test_1', api_error_key, {}).toHash(errorConfig).err.msg);
   });
 
   it('Should return internal_id in err as per input error code', function(){
 
     assert.equal('test_1',
-      responseHelper.error('test_1', api_error_key, {}).toHash().err.internal_id);
+      responseHelper.error('test_1', api_error_key, {}).toHash(errorConfig).err.internal_id);
   });
 
   it('Should have key count of 3 in response hash', function(){
 
-    assert.equal(3,
-      Object.keys(responseHelper.error('test_1', api_error_key, {}).toHash()).length);
+    assert.equal(2,
+      Object.keys(responseHelper.error('test_1', api_error_key, {}).toHash(errorConfig)).length);
   });
 
   it('Should have key count of 4 in err object of response hash', function(){
 
     assert.equal(4,
-      Object.keys(responseHelper.error('test_1', api_error_key, {}).toHash().err).length);
+      Object.keys(responseHelper.error('test_1', api_error_key, {}).toHash(errorConfig).err).length);
   });
 
   it('Should return name in param object as per code in input', function(){
 
-    assert.equal(paramErrorConfig.user_name_inappropriate.name,
-      responseHelper.paramValidationError('test_1', api_error_key, [param_error_key], {}).toHash().err.param[0].name);
+    assert.equal(paramErrorConfig.test_1.name,
+      responseHelper.paramValidationError('test_1', api_error_key, [param_error_key], {}).toHash(errorConfig).err.param[0].name);
   });
 
   it('Should return message in param object as per code in input', function(){
 
-    assert.equal(paramErrorConfig.user_name_inappropriate.message,
-      responseHelper.paramValidationError('test_1', api_error_key, [param_error_key], {}).toHash().err.param[0].msg);
+    assert.equal(paramErrorConfig.test_1.message,
+      responseHelper.paramValidationError('test_1', api_error_key, [param_error_key], {}).toHash(errorConfig).err.param[0].msg);
   });
 
   it('Should return a Result object', function(){
