7a7abb4275af9377f17dcb9a027a9878b8fca0a0
Rachin Kapoor <rachin@ost.com>
2018-05-09 09:39:30 +0530

    Rachin/ostweb3/testcases (#17)
    * OA-24 - Write Mocha Test Cases for OSTWeb3.
    
    * Fix - getWeb3 should also take poolConfig.
    
    * OA-29 : Write Mocha Test Cases for PromiseQueueManager.
    
    * OA-64 : Mocha Test Cases for Web3 WebSocket Pool
    
    * geth_manager.js : A Helper class for test cases that allows you to start/stop geth.
    
    * setup_geth.js : Setup Geth for test cases. 
    
    * travis config and get setup

---
+++ tests/helpers/geth_manager.js
@@ -0,0 +1,371 @@
+"use strict";
+
+
+const { spawn }         = require('child_process')
+    , path              = require("path")
+    , rootPrefix        = "../.."
+    , start_time_buffer = 15000
+    , stop_time_buffer  = 5000
+    , init_time_buffer  = 20000
+;
+
+const gethArgs = {
+  networkid         : "20171010"
+  , datadir         : path.resolve(__dirname, rootPrefix + "/tests/scripts/st-poa")
+  , port            : "30301"
+  , maxpeers        : "0"
+  , verbosity       : "3"
+  
+  // ACCOUNT OPTIONS
+  , unlock          : null
+  , password        : path.resolve(__dirname, rootPrefix + "/tests/scripts/pw" )
+  
+  // MINER OPTIONS
+  , mine            : ""
+  , minerthreads    : "4"
+  , etherbase       : null
+  , targetgaslimit  : "100000000"
+  , gasprice        : "1"
+
+
+  //RPC-CONFIG
+  , rpc       : ""
+  , rpcapi    : "eth,net,web3,personal,txpool"
+  , rpcport   : "12546"
+  , rpcaddr   : "127.0.0.1"
+
+
+  //WS-CONFIG
+  , ws        : ""
+  , wsport    : "13546"
+  , wsorigins : "*"
+  , wsaddr    : "127.0.0.1"
+  , wsapi     : "eth,net,web3,personal,txpool"
+};
+
+const gethSetupConfig = {
+  poaGenesisAbsolutePath  : path.resolve(__dirname, rootPrefix + "/tests/scripts/poa-genesis.json")
+  , preInitArgsToIgnore   : ["etherbase", "unlock", "password", "mine", "minerthreads"]
+  , passphrase            : "testtest"
+  , noOfAddresses         : 3
+  , datadir               : gethArgs.datadir
+  , passphraseFilePath    : gethArgs.password
+};
+
+const gethSpawnOptions = {
+  // stdio : [ 'ignore', process.stdout, process.stderr ]
+};
+
+const GethManager = function () {
+  const oThis = this;
+
+  oThis.gethArgs          = Object.assign( {}, gethArgs );
+  oThis.gethSetupConfig   = Object.assign( {}, gethSetupConfig );
+  oThis.gethSpawnOptions  = Object.assign( {}, gethSpawnOptions );
+  oThis.bindSignalHandlers();
+
+};
+
+GethManager.prototype = {
+  constructor: GethManager
+  , gethArgs  : null
+  , gethProcess: null
+  , isAlive: function () {
+    const oThis = this;
+    if ( !oThis.gethProcess ) {
+      console.log("[GETH-isAlive] oThis.gethProcess is null");
+      return false;
+    }
+    console.log("[GETH-isAlive] oThis.gethProcess.killed", oThis.gethProcess.killed);
+    return !oThis.gethProcess.killed;
+  }
+
+  , _startPromise : null
+  , startWaitTime : 5000
+  , gethSpawnOptions: null
+  , start: function ( argKeysToIgnore ) {
+    const oThis = this;
+
+
+    oThis._startPromise = oThis._startPromise || new Promise( function ( resolve, reject) {
+        if ( oThis.isAlive() ) {
+          console.log("[GETH-START] gethProcess.pid =", oThis.gethProcess.pid, "Resolving the Start Promise");
+          resolve( true );
+        }
+
+        let gethArgsArray = []
+          , argKey
+          , argValue
+        ;
+
+
+        argKeysToIgnore = argKeysToIgnore || [];
+        for( argKey in oThis.gethArgs ) {
+          if ( !( oThis.gethArgs.hasOwnProperty( argKey ) ) )  {
+            continue;
+          }
+          if ( argKeysToIgnore.indexOf( argKey ) >= 0 ) {
+            //Ignore this arg.
+            continue;
+          }
+
+          //Push the key
+          gethArgsArray.push( "--" + argKey );
+
+          argValue = oThis.gethArgs[ argKey ];
+
+          if ( !argValue ) {
+            if ( argKey === "unlock" || argKey === "etherbase" ) {
+              //Minner Address is missing.
+              let addressInfo = oThis.getTransactionAddressInfo();
+              argValue = addressInfo.minner || "0";              
+            }
+          }
+
+          if ( argValue && argValue.length ) {
+            //Push the value.
+            gethArgsArray.push( argValue );
+          }
+        }
+
+        console.log("\t [GETH-START] Starting geth with command :: \ngeth", gethArgsArray.join(" "), "\n");
+
+        let gethProcess = oThis.gethProcess = spawn("geth", gethArgsArray, oThis.gethSpawnOptions );
+        gethProcess.on("close", function (code, signal) {
+          console.log("\t [GETH-START] gethProcess has exitted!   code:", code, "signal:", signal, "pid:", gethProcess.pid, "killed:", gethProcess.killed, "geth command:\n geth", gethArgsArray.join(" "), "\n");
+
+          oThis.gethProcess = null;
+        });
+
+
+        // Give some time to geth to start.
+        setTimeout( function () {
+
+          if ( oThis.isAlive() ) {
+
+            console.log("[GETH-START] gethProcess.pid =", oThis.gethProcess.pid, "Resolving the Start Promise");
+            resolve( true );  
+          } else {
+            reject(new Error("Failed to start geth.") );
+          }
+          
+          let gethProcessIdArray = [ "aux", "|", "grep", "'geth'", "|", "awk", "'{print $2,$11,$12}'" ];
+          let gethProcessIdFinder = spawn("ps", gethProcessIdArray, {shell: true, stdio: [ 'ignore', process.stdout, process.stderr ] });
+          
+        }, start_time_buffer );
+        
+      })
+      .then( function () {
+        // Finally, _startPromise should be set to null.
+        oThis._startPromise = null;
+      })
+      .catch( function ( reason ) {
+        // Ensure gethProcess becomes null.
+        oThis.gethProcess = null;
+        oThis._startPromise = null;
+        throw reason;
+      })
+    ;
+    return oThis._startPromise;
+  }
+
+  , _stopPromise: null
+  , stop: function () {
+    const oThis = this;
+
+
+    oThis._stopPromise = oThis._stopPromise || new Promise( function ( resolve, reject) {
+        if ( !oThis.isAlive() ) {
+          resolve( true );
+        }
+
+        let gethProcessIdArray = [ "aux", "|", "grep", "'geth'", "|", "awk", "'{print $2,$11,$12}'" ];
+        console.log("\t [GETH-STOP] oThis.gethProcess.pid = ", oThis.gethProcess.pid);
+        let gethProcessIdFinder = spawn("ps", gethProcessIdArray, {shell: true, stdio: [ 'ignore', process.stdout, process.stderr ] });
+
+        oThis.gethProcess.kill("SIGINT");
+
+        
+        // This is dummy code.
+        setTimeout( function () {
+          if ( !oThis.isAlive() ) {
+            resolve( true );  
+          } else {
+            reject(new Error("Failed to stop geth.") );
+          }
+        }, stop_time_buffer );
+      })
+      .then( function () {
+        // Finally, _startPromise should be set to null.
+        oThis._stopPromise = null;   
+      })
+      .catch( function ( reason ) {
+        oThis._stopPromise = null;
+        throw reason;
+      })
+    ;
+
+    return oThis._stopPromise;
+  }
+
+  , getWebSocketEndPoint: function () {
+    const oThis     = this
+        , gethArgs  = oThis.gethArgs
+    ;
+
+    return "ws://" + gethArgs["wsaddr"] + ":" + gethArgs["wsport"];
+  }
+
+  , getHttpEndPoint: function () {
+    const oThis     = this
+        , gethArgs  = oThis.gethArgs
+    ;
+
+    return "http://" + gethArgs["rpcaddr"] + ":" + gethArgs["rpcport"];
+  }
+  , bindSignalHandlers: function () {
+    const oThis = this;
+
+    const sigHandler = function () {
+      console.log("\t [GETH-SIGHANDLER] GethManager :: sigHandler triggered!. Stoping Geth Now.");
+      oThis.stop();
+    };
+
+    process.on('SIGINT', sigHandler);
+    process.on('SIGTERM', sigHandler);
+
+  }
+  , __sender            : null
+  , __senderPassphrase  : null
+  , __recipient         : null
+  , __minner            : null
+  , getTransactionAddressInfo: function () {
+    const oThis = this;
+    if ( !oThis.__sender || !oThis.__senderPassphrase || !oThis.__recipient ) {
+      oThis.populateTransactionAddressInfo();
+    }
+    // This is dummy code.
+    return {
+      sender        : oThis.__sender
+      , passphrase  : oThis.__senderPassphrase
+      , recipient   : oThis.__recipient
+      , minner      : oThis.__minner
+    };
+  }
+  , populateTransactionAddressInfo: function () {
+    const oThis = this
+        , gethSetupConfig         = oThis.gethSetupConfig
+        , poaGenesisAbsolutePath  = gethSetupConfig.poaGenesisAbsolutePath
+        , addresses     = ["__minner", "__sender", "__recipient"]
+    ;
+
+    let poaGenesis    = require( poaGenesisAbsolutePath )
+      , genesisAlloc  = poaGenesis.alloc
+      , addrLen       = addresses.length
+      , gKey
+    ;
+
+    for( gKey in genesisAlloc ) {
+      if ( !( genesisAlloc.hasOwnProperty( gKey ) ) ) {
+        continue;
+      }
+      if ( !addrLen-- ) {
+        break;
+      }
+      oThis[ addresses[addrLen] ] = gKey;
+    }
+
+
+    addrLen++;
+    while( addrLen-- && gKey ) {
+      // Assign the last address.
+      oThis[ addresses[addrLen] ] = gKey;
+    }
+
+
+    oThis.__senderPassphrase = gethSetupConfig.passphrase;
+  }
+  , gethSetupConfig : null
+  , getGethSetupConfig: function () {
+    const oThis = this;
+    return oThis.gethSetupConfig;
+  }
+
+  , __initPromise: null
+  , hasInitialized: false
+  , initGeth: function () {
+    const oThis = this;
+
+    oThis.__initPromise = oThis.__initPromise || new Promise( function( resolve, reject ) {
+      if ( oThis.isAlive() ) {
+        reject( new Error( "Geth already running. Can not initialize it" ) );
+      }
+
+      let gethArgs        = oThis.gethArgs
+        , gethSetupConfig = oThis.gethSetupConfig
+        , rmArgsArray     = [ "-rf"
+          , gethArgs["datadir"] + "/geth/*"
+        ]
+        , gethArgsArray   = [ 
+          "init"
+          , "--datadir"
+          , gethArgs["datadir"]
+          , gethSetupConfig["poaGenesisAbsolutePath"]
+        ]
+      ;
+
+      let gethProcess
+        , gethExitCode
+      ;
+
+
+      // Clean up file.
+      console.log("\t [Geth-Init] removing geth folder from datadir.");
+      let removeFilesProcess = spawn("rm", rmArgsArray, {shell: true, stdio: [ 'ignore', process.stdout, process.stderr ] });
+      removeFilesProcess.on("close", function (code, signal) {
+        console.log("\t [Geth-Init] removed geth folder from datadir. code:", code, "signal:", signal);
+
+        // Now init geth.
+        gethProcess = oThis.gethProcess = spawn("geth", gethArgsArray, oThis.gethSpawnOptions );
+        gethExitCode = "STILL_RUNNING";
+
+        gethProcess.on("close", function (code, signal) {
+          console.log("\t [Geth-Init] gethProcess has exitted!  code:", code, "signal", signal, "pid", gethProcess.pid, "killed", gethProcess.killed, "geth command:\n geth", gethArgsArray.join(" "), "\n");
+          if ( !code ) {
+            gethExitCode = "EXIT_WITHOUT_ERROR";
+          } else {
+            gethExitCode = "EXIT_WITH_ERROR_CODE_" + String ( code );
+          }
+          oThis.gethProcess = null;
+        });
+
+        // Give some time to geth to start.
+        setTimeout(function () {
+          oThis.__initPromise = null;
+          switch( gethExitCode ) {
+            case "EXIT_WITHOUT_ERROR":
+              resolve( true );
+              break;
+            case "STILL_RUNNING":
+              reject( new Error("Failed to initialize geth. Timeout Error.") );
+              break;
+            default:
+              reject( new Error("Failed to initialize geth. gethExitCode " + gethExitCode) );
+              break;
+          }
+        }, init_time_buffer);
+
+      });
+
+
+
+    });
+    return oThis.__initPromise;
+  }
+};
+
+
+
+
+
+module.exports = new GethManager();
---
+++ tests/mocha/lib/ost_web3/ost-web3.js
@@ -0,0 +1,349 @@
+"use strict";
+
+// Load external packages
+const Chai            = require('chai')
+    , assert          = Chai.assert
+    , rootPrefix      = "../../../.."
+    , OSTBase         = require( rootPrefix + "/index" )
+    , Logger          = OSTBase.Logger
+    , Web3            = OSTBase.Web3
+    , OstWeb3         = OSTBase.OstWeb3
+    
+    , gethManager     = require( rootPrefix + "/tests/helpers/geth_manager")
+    , logger          = new Logger( "OstWeb3TestCases", Logger.LOG_LEVELS.INFO )
+
+    // Provider classes.
+    , HttpProvider      = require("web3-providers-http")
+    , WebsocketProvider = require("web3-providers-ws")
+    , OstWSProvider     = OSTBase.OstWeb3.OstWSProvider
+    
+    // End-Points
+    , httpEndPoint      = gethManager.getHttpEndPoint()
+    , wsEndPoint        = gethManager.getWebSocketEndPoint()
+
+    , describePrefix    = "lib/ost_web3/ost-web3"
+;
+
+
+
+
+// Some Constants. All times are in milliseconds.
+const avg_block_time              = 5000    /* Avg time required to mine a block */
+    , no_of_conformation_blocks   = 4 + 6   /* We expect receipt of transactions to be received in these many blocks. */
+    , buffer_time_per_describe    = 5000
+    , max_time_per_transaction    = (avg_block_time * no_of_conformation_blocks) + buffer_time_per_describe
+    , max_time_for_geth_start     = 20000 /* Time Required for geth to start */
+    , max_time_for_geth_stop      = 10000 /* Time Required for geth to stop  */
+    , amt_to_transfer_in_eth = "0.01"
+; 
+
+// This is the main function. Let it execute once all methods are defined.
+const mainFn = function () { 
+  // add all test cases.
+  let testGroups = [];
+  testGroups.push( startGethTestGroup );
+  testGroups.push( createAndValidateWeb3Instances );
+  testGroups.push( sendTransactionTestGroup );
+  testGroups.push( stopGethTestGroup );
+  testGroups.push( startGethTestGroup );
+  testGroups.push( sendTransactionTestGroup );
+  testGroups.push( stopGethTestGroup );
+
+  describe(describePrefix, function () {
+    let testCases
+      , len       
+      , cnt
+      , testCase
+    ;
+
+    while( testGroups.length ) {
+      testCases = testGroups.shift()();
+    }
+
+    // Finally - Have a nice day.
+    it("will say have a nice day", function () {
+      logger.win("Have a nice day! The process will exit in 1 second.");
+      assert.isOk( true );
+      setTimeout( function () {
+        process.exit(0);
+      }, 1000);
+    })
+  });
+};
+
+let basic_transaction_info = null;
+
+const expectedOutValues = {
+  didUnlock                   : true
+  , callbackTriggered         : true
+  , callbackError             : null
+  , transactionHashEvent      : true
+  , receiptEvent              : true
+  , confirmationEvent         : true
+  , confirmationEventNumber   : 6
+  , errorEvent                : false
+  , didResolveTxPromise       : true
+  , receiptStatus             : "0x1"
+};
+
+// Test-Case Builder.
+const sendTransactionWith = function ( web3 ) {
+  basic_transaction_info = basic_transaction_info || gethManager.getTransactionAddressInfo();
+
+  let outValues = {
+      didUnlock                   : false
+
+      , callbackTriggered         : false
+      , callbackError             : null
+      , callbackHash              : null
+
+      , transactionHashEvent      : false
+      , transactionHashEventValue : null
+
+      , receiptEvent              : false
+      , receiptEventHash          : null
+
+      , confirmationEvent         : false
+      , confirmationEventNumber   : -1
+      , confirmationEventHash     : null
+
+      , errorEvent                : false
+
+      , didResolveTxPromise       : false
+    }
+    , sender      = basic_transaction_info.sender
+    , passphrase  = basic_transaction_info.passphrase
+    , txParams    = {
+        from: sender
+        , to: basic_transaction_info.recipient
+        , value : web3.utils.toWei( amt_to_transfer_in_eth )
+        , gas: "50000"
+    }
+  ;
+
+
+  let executionPromise = web3.eth.personal.unlockAccount( sender, passphrase )
+    .then( function () {
+      outValues.didUnlock = true;
+      return web3.eth.sendTransaction( txParams , function (error, hash) {
+          outValues.callbackTriggered = true;
+          outValues.callbackError = error;
+          outValues.callbackHash = hash;
+        })
+        .on("transactionHash", function ( hash ) {
+          outValues.transactionHashEvent      = true;
+          outValues.transactionHashEventValue = hash;
+        })
+        .on("receipt", function ( receipt ) {
+          receipt = receipt || {};
+          outValues.receiptEvent = true;
+          outValues.receiptEventHash = receipt.transactionHash;
+          logger.info("receipt", receipt);
+        })
+        .on("confirmation", function (confirmationNumber, receipt) {
+
+          if ( confirmationNumber != expectedOutValues.confirmationEventNumber ) {
+            // Ignore this event.
+            return;
+          }
+          receipt = receipt || {};
+          outValues.confirmationEvent       = true;
+          outValues.confirmationEventNumber = confirmationNumber;
+          outValues.confirmationEventHash   = receipt.transactionHash;
+
+        })
+        .on("error", function ( error ) {
+          outValues.errorEvent = true;
+        })
+    })
+    .then( function ( receipt ) {
+      receipt = receipt || {};
+      outValues.didResolveTxPromise = true;
+      outValues.receiptStatus = receipt.status;
+      return outValues;
+    })
+
+  return executionPromise
+    .catch( function ( reason ) {
+      //Catch any exception and let the flow continue.
+      logger.error("executionPromise failed with reason:", reason);
+      return outValues;
+    })
+    .then( function (i_d_k) {
+      return outValues;
+    })
+  ;
+};
+
+const verifyResult = function ( result ) {
+  result = result || {};
+  let eKey;
+  for( eKey in expectedOutValues ) {
+    if ( !expectedOutValues.hasOwnProperty( eKey ) ) {
+      continue;
+    }
+
+    assert.isTrue( result.hasOwnProperty( eKey ), "result is missing " + eKey + " property" );
+    assert.equal( result[eKey], expectedOutValues[ eKey ], "result does not match expected output. Property : " + eKey);
+  }
+
+  assert.isOk( result.callbackHash, "callbackHash is not ok.");
+  assert.equal( result.callbackHash, result.transactionHashEventValue, "callbackHash does not match transactionHashEventValue");
+  assert.equal( result.receiptEventHash, result.transactionHashEventValue, "receiptEventHash does not match transactionHashEventValue");
+  assert.equal( result.confirmationEventHash, result.transactionHashEventValue, "confirmationEventHash does not match transactionHashEventValue");
+
+
+  return true;
+};
+
+const startGethTestGroup = function () {
+  let validator = function () { 
+    this.timeout( max_time_for_geth_start );
+    logger.step("Start Geth TestGroup");
+    return gethManager
+      .start()
+      .then( function () {
+        logger.info("Geth started successfully.");
+        assert.isOk(true);
+      })
+      .catch( function ( reason ) {
+        logger.error("Failed to start geth. reason", reason);
+        assert.isOk(false, "Failed to start geth.");
+      })
+    ;
+  };
+
+  it("should start geth.", validator);
+};
+
+// Web3 Instances. Make sure to define keys before hand in web3Instances.
+let web3Instances = {
+    web3WithHttp      : null
+    , ostWeb3WithHttp : null
+    , web3WithWS      : null
+    , ostWeb3WithWS   : null
+  }
+  , ostWeb3WithHttp
+  , ostWeb3WithWS
+  , web3WithHttp
+  , web3WithWS
+;
+
+const createAndValidateWeb3Instances = function () {
+  
+  it("should create new web3 instances.", function () {
+    web3Instances.web3WithHttp    = web3WithHttp    = new Web3( httpEndPoint );
+    web3Instances.ostWeb3WithHttp = ostWeb3WithHttp = new OstWeb3( httpEndPoint );
+    web3Instances.web3WithWS      = web3WithWS      = new Web3( wsEndPoint );
+    web3Instances.ostWeb3WithWS   = ostWeb3WithWS   = new OstWeb3( wsEndPoint, null, {
+      providerOptions: {
+        killOnReconnectFailure: false
+      }
+    });
+    assert.isOk(true);
+  });
+
+  it("web3WithHttp.currentProvider should be an instance of HttpProvider", function () {
+    assert.instanceOf( web3WithHttp.currentProvider, HttpProvider, "web3WithHttp has incorrect provider set");    
+  });
+  it("ostWeb3WithHttp.currentProvider should be an instance of HttpProvider", function () {
+    assert.instanceOf( ostWeb3WithHttp.currentProvider, HttpProvider, "ostWeb3WithHttp has incorrect provider set");    
+  });
+  it("web3WithWS.currentProvider should be an instance of WebsocketProvider", function () {
+    assert.instanceOf( web3WithWS.currentProvider, WebsocketProvider, "web3WithWS has incorrect provider set");  
+  });
+  it("ostWeb3WithWS.currentProvider should be an instance of OstWSProvider", function () {
+    assert.instanceOf( ostWeb3WithWS.currentProvider, OstWSProvider, "ostWeb3WithWS has incorrect provider set");    
+  });
+};
+
+const sendTransactionTestGroup = function () { 
+  
+  let web3OutValues = {}
+    , web3Key
+    , validator
+  ;
+
+  // Initiate Transactions.
+  validator = function () {
+    this.timeout( max_time_per_transaction );
+    let web3Key;
+    for( web3Key in web3Instances ) {
+      if ( !web3Instances.hasOwnProperty( web3Key ) ) {
+        continue;
+      }
+
+      // Create a closure for web3Key and call sendTransactionWith.
+      ( function ( web3Key ) {
+        let currWeb3 = web3Instances[ web3Key ];
+        logger.info("Testing sendTransaction with", web3Key);
+        sendTransactionWith( currWeb3 )
+          .then( function ( outValues ) {
+            web3OutValues[ web3Key ] = outValues;
+          })
+        ;
+      })( web3Key );
+
+    } // End of for loop.
+    assert.isOk( true );
+  };
+  it("should initiate send transaction flows for all instances of web3.", validator);
+  
+  // Verify Transaction results.
+  let validateAfter = max_time_per_transaction + 2000;
+  for( web3Key in web3Instances ) {
+    if ( !web3Instances.hasOwnProperty( web3Key ) ) {
+      continue;
+    }
+
+    validator = (function ( web3Key, validateAfter ) { 
+
+      return function () {
+        this.timeout( validateAfter + 1000 );
+
+        if ( !web3Instances[ web3Key ] ) {
+          logger.info("Validation of web3 Instance", web3Key, "has been skipped.");
+          assert.isOk( true );
+          return;
+        }
+
+        return new Promise( function ( resolve, reject ) {
+          setTimeout( function () {
+            resolve( web3OutValues[ web3Key ] )
+          }, validateAfter);
+        })
+        .then( function ( outValues ) {
+          verifyResult( outValues, web3Key );
+        })        
+      };
+
+    })( web3Key, validateAfter );
+    it("should validate send transaction outputs with " + web3Key + ". timeout for this test case set to " + validateAfter, validator);
+    validateAfter = 1000;
+  }
+};
+
+
+const stopGethTestGroup = function () {
+  let validator = function () { 
+    this.timeout( max_time_for_geth_stop );
+    return gethManager
+      .stop()
+      .then( function () {
+        if ( web3Instances.hasOwnProperty( "web3WithWS" )  ) {
+          logger.info("Removing web3WithWS from web3Instances as it will not re-connect.");
+          delete web3Instances.web3WithWS;
+        }
+        assert.isOk(true);
+      })
+      .catch( function () {
+        assert.isOk(false, "Failed to stop geth.");
+      })
+    ;
+  };
+
+  it("should stop geth.", validator);
+};
+
+// Start the test.
+mainFn();
