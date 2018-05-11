2bf76cdb6fdabc1a3fb6a160879f5ccbbaa8f04e
abhay-pepo <abhay@pepo.com>
2018-05-11 19:52:41 +0530

    [OA-84] CreateTableMigration Done

--- services/dynamodb/create_table_migration.js
+++ services/dynamodb/create_table_migration.js
@@ -23,48 +23,30 @@ const rootPrefix  = "../.."
  * Constructor for TableExist service class
  *
  * @params {object} ddbObject - DynamoDB Object
- * @params {object} params - CreateTableMigration params
- * @params {object} params.auto_scale_object - auto scale object
- * @params {string} TableName - name of table
+ * @params {object} params - params
+ * @params {object} params.createTableConfig - create table configurations
+ * @params {object} params.updateContinuousBackupConfig - update Continuous Backup configurations
+ * @params {object} params.autoScalingConfig - scaling params
+ * @params {object} params.autoScalingConfig.registerScalableTargetWrite - register Scalable Target write configurations
+ * @params {object} params.autoScalingConfig.registerScalableTargetRead - register Scalable Target read configurations
+ * @params {object} params.autoScalingConfig.putScalingPolicyWrite- Put scaling policy write configurations
+ * @params {object} params.autoScalingConfig.putScalingPolicyRead - Put scaling policy read configurations
  *
  * @constructor
  */
 const CreateTableMigration = function(ddbObject, params) {
   const oThis = this
   ;
-  oThis.autoScaleObject = params.auto_scale_object;
+  oThis.createTableConfig = params.createTableConfig;
+  oThis.updateContinuousBackupConfig = params.updateContinuousBackupConfig;
+  oThis.autoScalingConfig = params.autoScalingConfig;
+
   DDBServiceBaseKlass.call(oThis, ddbObject, 'createTableMigration', params);
 };
 
 CreateTableMigration.prototype = Object.create(DDBServiceBaseKlass.prototype);
 
 const CreateTableMigrationPrototype = {
-
-  /**
-   * Perform method
-   *
-   * @return {promise<result>}
-   *
-   */
-  perform: async function () {
-    const oThis = this
-    ;
-    try {
-      var r = null;
-      r = oThis.validateParams();
-      logger.debug("=======TableExist.validateParams.result=======");
-      logger.debug(r);
-      if (r.isFailure()) return r;
-
-      r = await oThis.checkTableExist();
-      logger.debug("=======TableExist.checkTableExist.result=======");
-      logger.debug(r);
-      return r;
-    } catch (err) {
-      logger.error("services/dynamodb/table_exists.js:perform inside catch ", err);
-      return responseHelper.error('s_dy_te_perform_1', 'Something went wrong. ' + err.message);
-    }
-  },
 
   /**
    * Validation of params
@@ -78,7 +60,33 @@ const CreateTableMigrationPrototype = {
     ;
     if (baseValidationResponse.isFailure()) return baseValidationResponse;
 
-    if (!oThis.params.TableName) return responseHelper.error('l_dy_te_validateParams_1', 'TableName is mandatory');
+    if (!oThis.params.createTableConfig) {
+      return responseHelper.error('l_dy_ctm_validateParams_1', 'create table config is mandatory');
+    }
+
+    if (!oThis.params.updateContinuousBackupConfig) {
+      return responseHelper.error('l_dy_ctm_validateParams_2', 'updateContinuousBackupConfig config is mandatory');
+    }
+
+    if (!oThis.params.autoScalingConfig){
+      return responseHelper.error('l_dy_ctm_validateParams_3', 'auto scaling configs are mandatory');
+    }
+
+    if (!oThis.params.autoScalingConfig.registerScalableTargetWrite){
+      return responseHelper.error('l_dy_ctm_validateParams_4', 'auto scaling registerScalableTargetWrite config is mandatory');
+    }
+
+    if (!oThis.params.autoScalingConfig.registerScalableTargetRead){
+      return responseHelper.error('l_dy_ctm_validateParams_5', 'auto scaling registerScalableTargetRead config is mandatory');
+    }
+
+    if (!oThis.params.autoScalingConfig.putScalingPolicyWrite){
+      return responseHelper.error('l_dy_ctm_validateParams_6', 'auto scaling putScalingPolicyWrite config is mandatory');
+    }
+
+    if (!oThis.params.autoScalingConfig.putScalingPolicyRead){
+      return responseHelper.error('l_dy_ctm_validateParams_7', 'auto scaling putScalingPolicyRead config is mandatory');
+    }
 
     return responseHelper.successWithData({});
   },
@@ -93,51 +101,65 @@ const CreateTableMigrationPrototype = {
    */
   executeDdbRequest: function() {
     const oThis = this
-      , registerAutoScalePromiseArray = []
-      , putAutoScalePolicyArray = []
-    ;
+      ;
     return new Promise(async function (onResolve) {
 
       logger.info("Creating table..");
-      const createTableResponse = await oThis.ddbObject.call('createTable', oThis.params);
+      const createTableResponse = await oThis.ddbObject.call('createTable', oThis.createTableConfig);
       if(createTableResponse.isFailure()){
-        throw "Failure in table creation"
+        return onResolve(createTableResponse);
       }
 
-      const roleARN = createTableResponse.data.TableDescription.TableArn;
+      const roleARN = createTableResponse.data.TableDescription.TableArn
+        , tableName = createTableResponse.data.tableName
+        , waitForTableExistsParams = {tableName: tableName}
+      ;
       logger.debug("Table arn :", roleARN);
 
       logger.info("Waiting for table creation..");
-      const responseOfWaitFor = await oThis.ddbObject.call('waitFor','tableExists', params);
-      if(responseOfWaitFor.isFailure()){
-        throw "Failure in wait for"
+      const waitFortableExistsResponse = await oThis.ddbObject.call('waitFor','tableExists', waitForTableExistsParams);
+      if(waitFortableExistsResponse.isFailure()){
+        return onResolve(waitFortableExistsResponse);
       }
 
       logger.info("Enable continuous backup..");
-      const continuousBackupResponse = await oThis.ddbObject.call('updateContinuousBackups', params);
+      const continuousBackupResponse = await oThis.ddbObject.call('updateContinuousBackups', oThis.updateContinuousBackupConfig);
       if(continuousBackupResponse.isFailure()){
-        throw "Failure in continuousBackupResponse"
+        return onResolve(continuousBackupResponse);
       }
 
       logger.info("Register auto scaling target..");
-      registerAutoScalePromiseResponse.push(oThis.autoScaleObject.registerScalableTarget(param1));
-      registerAutoScalePromiseResponse.push(oThis.autoScaleObject.registerScalableTarget(param2));
+      let registerAutoScalePromiseArray = []
+        , putAutoScalePolicyArray = []
+      ;
+      registerAutoScalePromiseArray.push(oThis.autoScalingConfig.registerScalableTargetWrite);
+      registerAutoScalePromiseArray.push(oThis.autoScalingConfig.registerScalableTargetRead);
 
       const registerAutoScalePromiseResponse = await Promise.all(registerAutoScalePromiseArray);
-      if (registerAutoScalePromiseResponse[0].isFailure() || registerAutoScalePromiseResponse[1].isFailure()) {
-        throw "Failure in register auto scale promise array"
+      if (registerAutoScalePromiseResponse[0].isFailure()) {
+        return onResolve(registerAutoScalePromiseResponse[0]);
+      }
+      if (registerAutoScalePromiseResponse[1].isFailure()) {
+        return onResolve(registerAutoScalePromiseResponse[1]);
       }
 
       logger.info("Putting auto scale policy..");
-      putAutoScalePolicyArray.push(oThis.autoScaleObject.putScalingPolicy(param1));
-      putAutoScalePolicyArray.push(oThis.autoScaleObject.putScalingPolicy(param2));
+      putAutoScalePolicyArray.push(oThis.autoScalingConfig.putScalingPolicyWrite);
+      putAutoScalePolicyArray.push(oThis.autoScalingConfig.putScalingPolicyRead);
 
       const putAutoScalePolicyPromiseResponse = await Promise.all(putAutoScalePolicyArray);
-      if (putAutoScalePolicyPromiseResponse[0].isFailure() || putAutoScalePolicyPromiseResponse[1].isFailure()) {
-        throw "Failure in put auto scale policy promise array"
+      if (putAutoScalePolicyPromiseResponse[0].isFailure()) {
+        return onResolve(putAutoScalePolicyPromiseResponse[0]);
+      }
+      if (putAutoScalePolicyPromiseResponse[1].isFailure()) {
+        return onResolve(putAutoScalePolicyPromiseResponse[1]);
       }
 
-      onResolve(data)
+      const describeTableParams = {tableName: tableName}
+        , describeTableResponse = await oThis.ddbObject.call('describeTable', describeTableParams)
+       ;
+
+      onResolve(describeTableResponse)
     });
   },
 
