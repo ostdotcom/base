ef80a936084ee65f7591dd33ab3ac22b408fa8d7
abhay-pepo <abhay@pepo.com>
2018-05-15 20:20:15 +0530

    [OA-137] auto scale test cases failed

--- tests/mocha/services/constants.js
+++ tests/mocha/services/constants.js
@@ -41,12 +41,12 @@ MochaTestConstants.prototype = {
    */
   DYNAMODB_CONFIGURATIONS_REMOTE : {
     'apiVersion': '2012-08-10',
-    'accessKeyId': 'AKIAJ6YMLEN7A7DQWGPQ',
-    'secretAccessKey': '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',
-    'region': 'us-east-1',
+    'accessKeyId': 'x',
+    'secretAccessKey': 'x',
+    'region': 'localhost',
     'logger': console,
     'sslEnabled': false,
-    'endpoint': "http://dynamodb.us-east-1.amazonaws.com"
+    'endpoint': "http://localhost:8000"
   },
 
   /**
@@ -56,13 +56,13 @@ MochaTestConstants.prototype = {
    *
    */
   AUTO_SCALE_CONFIGURATIONS_REMOTE : {
-    'apiVersion': '2016-02-06',
-    'accessKeyId': 'AKIAJ6YMLEN7A7DQWGPQ',
-    'secretAccessKey': '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',
-    'region': 'us-east-1',
+    'apiVersion': '2012-08-10',
+    'accessKeyId': 'x',
+    'secretAccessKey': 'x',
+    'region': 'localhost',
     'logger': console,
     'sslEnabled': false,
-    'endpoint': "http://autoscaling.us-east-1.amazonaws.com"
+    'endpoint': "http://localhost:8000"
   },
 
   transactionLogsTableName: 'shard_00001_transaction_logs'
