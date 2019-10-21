38937c58cf949bd4dbd6f5d4688b50bd3fada4df
santhu210 <santhosh.nitdgp2015@gmail.com>
2019-10-21 17:05:03 +0530

    Web3 cleanup and node upgrade changes

--- index.js
+++ index.js
@@ -5,14 +5,9 @@
 'use strict';
 
 const rootPrefix = '.',
-  Web3 = require('web3'),
-  OstWeb3 = require(rootPrefix + '/lib/ost_web3/ost-web3'),
-  //, OstWSProvider   = require( rootPrefix + '/lib/ost_web3/ost-web3-providers-ws' )
   Logger = require(rootPrefix + '/lib/logger/custom_console_logger'),
   PromiseContext = require(rootPrefix + '/lib/promise_context/promise_context'),
   PCQueueManager = require(rootPrefix + '/lib/promise_context/promise_queue_manager'),
-  Web3PoolFactory = require(rootPrefix + '/lib/web3_pool/ost_web3_pool_factory'),
-  Web3Pool = require(rootPrefix + '/lib/web3_pool/ost_web3_pool'),
   responseHelper = require(rootPrefix + '/lib/formatter/response_helper'),
   InstanceComposer = require(rootPrefix + '/lib/InstanceComposer');
 
@@ -20,8 +15,6 @@ const rootPrefix = '.',
 // All classes should begin with Capital letter.
 // All instances/objects should begin with small letter.
 module.exports = {
-  OstWeb3: OstWeb3,
-  Web3: Web3,
   logger: new Logger(),
   Logger: Logger,
   OSTPromise: {
@@ -29,10 +22,6 @@ module.exports = {
     QueueManager: PCQueueManager
   },
   responseHelper: responseHelper,
-  OstWeb3Pool: {
-    Factory: Web3PoolFactory,
-    Pool: Web3Pool
-  },
   InstanceComposer: InstanceComposer
 };
 
